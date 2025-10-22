a0bedecd8a9d529a401ced0e4a704278f081795b
Asaf Artsi <artsiasaf@gmail.com>
2025-10-22 15:38:24 +0300

    Added MongoDB to Server.js Process, Now Server is Linked to our MongoDB DB

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { getDb } = require("./db")
 const PORT = 3000;
+ 
+ // הגשת קבצי הלקוח
+ app.use('/client', express.static(path.join(__dirname, '../client')));
+ 
+ app.listen(PORT, () => {
+   console.log(`Server running on http://localhost:${PORT}`);
+ });
+ 
+ const router = require('./router');
+ app.use('/', router);
 
-// הגשת קבצי הלקוח
-app.use('/client', express.static(path.join(__dirname, '../client')));
+const testConnection = async () => {
+  try {
+    const db = await getDb();
+    const collections = await db.listCollections().toArray();
+    console.log("Collections:", collections.map(c => c.name));
+  } catch (err) {
+    console.error("❌ Mongo connection error:", err);
+  }
+};
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-const router = require('./router');
-app.use('/', router);
+testConnection();
