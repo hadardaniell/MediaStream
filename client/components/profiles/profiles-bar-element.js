3b67affa571121a5329ca483f8d9136f1dc6bd2f
Hadasalon10 <hadasal0n20@gmail.com>
2025-11-09 09:41:03 +0200

    Change in the add button - profiles-bar-elements.js

--- client/components/profiles/profiles-bar-element.js
+++ client/components/profiles/profiles-bar-element.js
@@ -150,12 +150,14 @@ class ProfilesBar extends HTMLElement {
         });
       });
 
-      const addBtn = this.shadowRoot.querySelector('.add-container');
-      if (addBtn) {
-        addBtn.addEventListener('click', () => {
-          alert('כאן נוסיף פרופיל חדש');
-        });
-      }
+    const addBtn = this.shadowRoot.querySelector('.add-container');
+    if (addBtn) {
+      addBtn.addEventListener('click', () => {
+        // מעבר לעמוד עריכת פרופיל
+        window.location.href = '/edit-profile';
+      });
+    }
+
     } catch (err) {
       console.error("Error loading profiles:", err);
     }
